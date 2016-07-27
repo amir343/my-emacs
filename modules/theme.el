be451999940d32f1446e6557ecbf325bb1500355
Amir Moulavi <amir.moulavi@klarna.com>
2016-07-27 09:13:05 +0200

    Finally found my desired theme

--- modules/theme.el
+++ modules/theme.el
@@ -5,8 +5,11 @@
 ;;(package-require 'moe-theme)
 ;;(load-theme 'moe-dark t)
 
-(package-require 'suscolors-theme)
-(load-theme 'suscolors t)
+;;(package-require 'suscolors-theme)
+;;(load-theme 'suscolors t)
+
+(package-require 'gruvbox-theme)
+(load-theme 'gruvbox t)
 
 ;; Provide feature
 (provide 'theme)
